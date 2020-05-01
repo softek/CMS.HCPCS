87ff698c7941b7ca9d6d3cfe9dad26b376cc46d1
Jeremy R. Sellars <jeremy.sellars@softekinc.com>
2020-05-01 08:39:10 -0500

    Update readme and samples for sources

---
+++ samples.cmd
@@ -0,0 +1,14 @@
+@echo off
+pushd %~dp0
+ECHO Building uberjar
+call build.cmd
+call :buildSample CSV         sample.csv
+call :buildSample JSON        sample.json
+call :buildSample SQL         sample.sql
+popd
+goto :EOF
+
+:buildSample
+ECHO ==========================================================================
+ECHO Building %1 -> %2
+java -jar target\com.softekpanther.cms-0.1.0-SNAPSHOT-standalone.jar %1 2019="january_2019_opps_web_addendum_b.12312018.xlsx" 2020="January 2020 Addendum B CORRECTION.02042020.xlsx" > %2
