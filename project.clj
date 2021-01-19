cc9eb1eef50ca61ef95282ab4f5c2bffae9b3bd6
Jeremy R. Sellars <jeremy.sellars@softekinc.com>
2021-01-19 12:01:24 -0600

    Docjure 1.14.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.softekpanther.cms "0.1.0-SNAPSHOT"
+(defproject com.softekpanther.cms "0.1.1-SNAPSHOT"
   :description "Read HCPCS codes from Excel sheet"
   :main com.softekpanther.cms.hcpcs
   :license {:name "Eclipse Public License"
@@ -6,6 +6,6 @@
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [org.clojure/data.json "1.0.0"]
                  [org.clojure/data.csv "1.0.0"]
-                 [dk.ative/docjure "1.13.0"]]
+                 [dk.ative/docjure "1.14.0"]]
 
   :profiles {:uberjar {:aot :all}})
