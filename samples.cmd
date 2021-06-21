2e9d5d3d61639dccc1e0a6b3df765986e5a085f4
Will Strohmeier <will.strohmeier@softekinc.com>
2021-06-21 09:31:29 -0500

    Added April_2021_OPPS_Web_Addendum_B.03222021.xlsx values.
    panther-14557

--- samples.cmd
+++ samples.cmd
@@ -16,4 +16,5 @@ java -jar target\com.softekpanther.cms-0.1.1-SNAPSHOT-standalone.jar %1 status-i
 2020="2020_january_web_addendum_b.12312019.xlsx" ^
 2020-04="January 2020 Addendum B CORRECTION.02042020.xlsx" ^
 2020-05-04="2020_april_web_addendum_b.05042020.xlsx" ^
-2021-01="2021_January_Web_Addendum_B.12.29.20.xlsx" > %2
+2021-01="2021_January_Web_Addendum_B.12.29.20.xlsx" ^
+2021-06="April_2021_OPPS_Web_Addendum_B.03222021.xlsx" > %2
