5b9d39576ad3a57857d4a7b1d690369bf9f9dda3
Will Strohmeier <will.strohmeier@softekinc.com>
2025-04-03 10:00:58 -0500

    Updated .xlsx file match, since the latest files use spaces not _.

--- samples.ps1
+++ samples.ps1
@@ -9,7 +9,7 @@ Function Convert-Addendum
     Write-Host "Building $Type -> $OutFile"
 
     $date = Get-Date -Format "yyyy-MM"
-    $addendum = Get-ChildItem "*addendum_b*.xlsx"
+    $addendum = Get-ChildItem "*addendum*b*.xlsx"
 
     java -jar target\com.softekpanther.cms-0.1.1-SNAPSHOT-standalone.jar $Type status-indicators.csv "$Date=$addendum" |
     Out-File -Path $OutFile -Encoding oem
