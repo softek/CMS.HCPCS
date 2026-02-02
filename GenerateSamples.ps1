8016215a10fbdcde002d1aa9beaece4999fc9353
Will Strohmeier <will.strohmeier@softekinc.com>
2026-02-02 08:48:25 -0600

    more code cleanup.

--- GenerateSamples.ps1
+++ GenerateSamples.ps1
@@ -10,7 +10,7 @@ Function Convert-Addendum {
     $date = Get-Date -Format "yyyy-MM"
     $addendum = Get-ChildItem "*addendum*b*.xlsx"
 
-    java -jar target\com.softekpanther.cms-0.1.1-SNAPSHOT-standalone.jar $Type status-indicators.csv "$Date=$addendum" | Out-File -FilePath $OutFile -Encoding oem
+    java -jar target\com.softekpanther.cms-0.1.1-SNAPSHOT-standalone.jar $Type status-indicators.csv "$date=$addendum" | Out-File -FilePath $OutFile -Encoding oem
 }
 
 Write-Host "Building uberjar"
@@ -20,4 +20,5 @@ Convert-Addendum "JSON" "sample.json"
 Convert-Addendum "SQL" "sample.sql"
 
 # replace empty strings with nulls in JSON, see panther-17125 for more info.
-(Get-Content sample.json) -replace '"PaymentRate":\s*""', '"PaymentRate":null' | Set-Content sample.json
+#(Get-Content sample.json) -replace '"PaymentRate":\s*""', '"PaymentRate":null' | Set-Content sample.json
+#The latest version of the addendum did not have nulls, so this is not required anymore. Keeping incase it comes back.
